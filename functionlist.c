286b145709e7a4c484b4ca341903b1647991d97e
Jihed124 <benmohamed.jihed@esprit.tn>
2022-11-15 10:39:47 +0100

    electoral managment

--- functionlist.c
+++ functionlist.c
@@ -1,13 +1,14 @@
 #include<stdlib.h>
 #include"functionlist.h"
 #include<stdio.h>
+#include<string.h>
 
 int addelect( char * filename,electoral e)
 {
     FILE * f=fopen(filename, "a");
     if(f!=NULL)
     {
-        fprintf(f,"%s %d %d %d %d %d %s %d %s %s %s %d\n",e.ID,e.muncipality,e.day,e.month,e.year,e.orientation,e.IDH,e.pollingstation,e.IDC1,e.IDC2,e.IDC3,e.submit);
+        fprintf(f,"%s %d %d %d %d %d %s %d %s %s %s \n",e.ID,e.muncipality,e.day,e.month,e.year,e.orientation,e.IDH,e.pollingstation,e.IDC1,e.IDC2,e.IDC3);
         fclose(f);
         return 1;
     }
@@ -15,58 +16,59 @@ int addelect( char * filename,electoral e)
         return 0;
 }
 
-int modifyelect(char ID[10], electoral new, char * filename)
+int modifyelect(char * filename,char ID[10], electoral enew )
 {
     electoral e;
+    int tr=0;
     FILE * f=fopen(filename, "r");
     FILE * f2 =fopen("aux.txt", "w");
-    if(f==NULL || f2==NULL)
+    if(f==NULL && f2==NULL)
     {
-        return 0;
-    }
-    else
-    {
-        while(fscanf(f,"%s %d %d %d %d %d %s %d %s %s %s %d\n",e.ID,&e.muncipality,&e.day,&e.month,&e.year,&e.orientation,e.IDH,&e.pollingstation,e.IDC1,e.IDC2,e.IDC3,&e.submit)!=EOF)
+   
+  
+        while(fscanf(f,"%s %d %d %d %d %d %s %d %s %s %s ",e.ID,&e.muncipality,&e.day,&e.month,&e.year,&e.orientation,e.IDH,&e.pollingstation,e.IDC1,e.IDC2,e.IDC3)!=EOF)
         {
             if(strcmp(e.ID,ID)==0)
-                fprintf(f2,"%s %d %d %d %d %d %s %d %s %s %s %d\n",e.ID,e.muncipality,e.day,e.month,e.year,e.orientation,e.IDH,e.pollingstation,e.IDC1,e.IDC2,e.IDC3,e.submit);
+               { fprintf(f2,"%s %d %d %d %d %d %s %d %s %s %s \n",enew.ID,enew.muncipality,enew.day,enew.month,enew.year,enew.orientation,enew.IDH,enew.pollingstation,enew.IDC1,enew.IDC2,enew.IDC3);
+               
+		tr=1;}
             else
-
-                fprintf(f2,"%s %d %d %d %d %d %s %d %s %s %s %d\n",new.ID,new.muncipality,new.day,new.month,new.year,new.orientation,new.IDH,new.pollingstation,new.IDC1,new.IDC2,new.IDC3,new.submit);
+                fprintf(f2,"%s %d %d %d %d %d %s %d %s %s %s \n",e.ID,e.muncipality,e.day,e.month,e.year,e.orientation,e.IDH,e.pollingstation,e.IDC1,e.IDC2,e.IDC3);
+                
 
         }
-        fclose(f);
+  
+    }
+    fclose(f);
         fclose(f2);
         remove(filename);
         rename("aux.txt", filename);
-        return 1;
-    }
-
+        return tr;
 }
 
 int deleteelect(char id[10], char * filename)
 {
-    electoral e;
+    electoral e;int tr=0;
     FILE * f=fopen(filename, "r");
     FILE * f2 =fopen("aux.txt", "w");
-    if(f==NULL || f2==NULL)
-        return 0;
-    else
+    if(f==NULL && f2==NULL)
+        
     {
-        while(fscanf(f,"%s %d %d %d %d %d %s %d %s %s %s %d\n",e.ID,&e.muncipality,&e.day,&e.month,&e.year,&e.orientation,e.IDH,&e.pollingstation,e.IDC1,e.IDC2,e.IDC3,&e.submit)!=EOF)
+        while(fscanf(f,"%s %d %d %d %d %d %s %d %s %s %s ",e.ID,&e.muncipality,&e.day,&e.month,&e.year,&e.orientation,e.IDH,&e.pollingstation,e.IDC1,e.IDC2,e.IDC3)!=EOF)
         {
             if(strcmp(e.ID,id)==0)
-                fprintf(f2,"%s %d %d %d %d %d %s %d %s %s %s %d\n",e.ID,e.muncipality,e.day,e.month,e.year,e.orientation,e.IDH,e.pollingstation,e.IDC1,e.IDC2,e.IDC3,e.submit);
+                tr=1;
             else
 
-                fprintf(f2,"%s %d %d %d %d %d %s %d %s %s %s %d\n",e.ID,e.muncipality,e.day,e.month,e.year,e.orientation,e.IDH,e.pollingstation,e.IDC1,e.IDC2,e.IDC3,e.submit);
+                fprintf(f2,"%s %d %d %d %d %d %s %d %s %s %s \n",e.ID,e.muncipality,e.day,e.month,e.year,e.orientation,e.IDH,e.pollingstation,e.IDC1,e.IDC2,e.IDC3);
         }
-        fclose(f);
-        fclose(f2);
-        remove(filename);
-        rename("aux.txt", filename);
-        return 1;
+      
     }
+    fclose(f);
+    fclose(f2);
+    remove(filename);
+    rename("aux.txt", filename);
+    return tr;
 }
 
 electoral searchelect( char * filename,char ID[10])
@@ -76,14 +78,13 @@ electoral searchelect( char * filename,char ID[10])
     FILE * f=fopen(filename, "r");
     if(f!=NULL )
     {
-        while(fscanf(f,"%s %d %d %d %d %d %s %d %s %s %s %d\n",e.ID,&e.muncipality,&e.day,&e.month,&e.year,&e.orientation,e.IDH,&e.pollingstation,e.IDC1,e.IDC2,e.IDC3,&e.submit)!=EOF && tr==0)
-        {
+        while(tr==0 && fscanf(f,"%s %d %d %d %d %d %s %d %s %s %s ",e.ID,&e.muncipality,&e.day,&e.month,&e.year,&e.orientation,e.IDH,&e.pollingstation,e.IDC1,e.IDC2,e.IDC3)!=EOF)  {
            if(strcmp(ID,e.ID)==0)
                 tr=1;
         }
     }
     if(tr==0)
-        e.ID[0]=-1;
+        e.ID[0]='N';
     return e;
 
 }
