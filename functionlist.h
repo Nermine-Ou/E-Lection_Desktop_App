57271b17ffc3ec89b65b937817713f09a500de99
Jihed124 <benmohamed.jihed@esprit.tn>
2022-11-15 10:40:09 +0100

    electoral managment

--- functionlist.h
+++ functionlist.h
@@ -11,14 +11,14 @@ int orientation;
 char IDH[10];
 int pollingstation;
 char IDC1[10],IDC2[10],IDC3[10];
-int submit[10];
+
 }electoral;
 
 
 int addelect( char * filename , electoral e );
-int modifyelect(char ID[10], electoral new, char * filename);
+int modifyelect(char * filename,char ID[10], electoral enew );
 int deleteelect(char ID[10], char * filename);
-electoral searchelect( char * filename,char id[10]);
+electoral searchelect( char * filename,char ID[10]);
 
 
 #endif // FUNCTION_H_INCLUDED
